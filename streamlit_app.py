e81d912c53189e4cc7c827d189cb80e1b955ab49
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2024-06-09 08:55:48 +0530

    Remove testing variables

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import os
-from dotenv import load_dotenv
 
 st.set_page_config(page_title="Neurolisys", page_icon="🐢")
 
@@ -9,10 +7,6 @@ st.title("Neurolisys 🐢")
 st.write(
     "Welcome to Neurolisys! This is a platform that uses AI to analyze your learning style and personality."
 )
-
-load_dotenv()
-
-st.write(os.environ["GEMINI_API_KEY"])
 
 if st.button("Check your kids Learning Style"):
     st.switch_page("pages/1 - Info.py")
